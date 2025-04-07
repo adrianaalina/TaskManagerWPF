21a898ca4f8e7b0f993ae2d90e6028003de8ba5f
Adriana <adrirusu80@gmail.com>
2025-04-07 15:02:40 +0300

    Database Created

---
+++ TaskManagerWPF/Data/DatabaseHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Data.SQLite;
+using System.IO;
+
+namespace TaskManagerWPF.Data
+{
+    public static class DataBaseHelper
+    {
+        public static string ObțineCaleaBazei()
+        {
+            string folder = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Data");
+
+            if (!Directory.Exists(folder))
+                Directory.CreateDirectory(folder);
+
+            return Path.Combine(folder, "TaskManager.db");
+        }
+
+        
+    }
+}
---
+++ TaskManagerWPF/Data/DatabaseInitializer.cs
@@ -0,0 +1,39 @@
+﻿using System.Data.SQLite;
+using System.IO;
+
+namespace TaskManagerWPF;
+
+public static class DatabaseInitializer
+{
+    private static readonly string dbPath = Path.Combine(
+        Directory.GetParent(AppDomain.CurrentDomain.BaseDirectory).Parent.Parent.Parent.FullName,
+        "Data",
+        "taskmanager.db"
+        );
+    public static void InitializeDatabase()
+    {
+        if (!File.Exists(dbPath))
+        {
+            SQLiteConnection.CreateFile(dbPath);
+        }
+
+        using var connection = new SQLiteConnection($"Data Source={dbPath};Version=3;");
+        connection.Open();
+
+        string createTableQuery = @"
+            CREATE TABLE IF NOT EXISTS Taskuri (
+                Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                Titlu TEXT NOT NULL,
+                Descriere TEXT,
+                Deadline TEXT,
+                Prioritate TEXT,
+                Categorie TEXT,
+                Finalizat INTEGER DEFAULT 0
+            );";
+
+        using var command = new SQLiteCommand(createTableQuery, connection);
+        command.ExecuteNonQuery();
+    }
+
+    public static string GetDatabasePath() => dbPath; // opțional, util pentru debug
+}
